ee3f338969d2700be6e30eb92a242ed2a825028f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-01 09:24:49 -0400

    3rd exercise in stream-adventure

--- io.js
+++ io.js
@@ -1,1 +1,12 @@
 process.stdin.pipe(process.stdout);
+
+
+/*
+
+Take data from `process.stdin` and pipe it to `process.stdout`.
+
+With `.pipe()`. `process.stdin.pipe()` to be exact.
+
+Don't overthink this.
+
+*/
