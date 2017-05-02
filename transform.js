776a552e649d43c96175868b1878ab22c75ae8e1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-02 09:31:53 -0400

    4th exercise in stream-adventure

--- transform.js
+++ transform.js
@@ -1,5 +1,8 @@
 var through = require('through2');
 var stream = through(write, end);
+var buffer = buffer.toString();
+
+process.stdin.pipe(tr).pipe(process.stdout);
 
 function write (buffer, encoding, next) {
   this.push('I got some data: ' + buffer + '\n');
