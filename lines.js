7341886a3854224e6029026325d724622f726ca2
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-05 10:38:40 -0400

    5th exercise in stream-adventure

--- lines.js
+++ lines.js
@@ -1,4 +1,10 @@
+var through = require('through2');
+var tr = through(function (buffer, encoding, next) {
+        this.push(buffer.toString().toUpperCase());
+        next();
+    });
 
+process.stdin.pipe(tr).pipe(process.stdout);
 
 
 
