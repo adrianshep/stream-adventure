30f35a62e9ff20b45194f42eff0627d4b1d684dd
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-06 14:13:37 -0400

    5th exercise in stream-adventure

--- lines.js
+++ lines.js
@@ -6,7 +6,14 @@ var tr = through(function (buffer, encoding, next) {
 
 process.stdin.pipe(tr).pipe(process.stdout);
 
-
+var split = require('split');
+process.stdin
+    .pipe(split())
+    .pipe(through2(function (line, _, next) {
+        console.dir(line.toString());
+        next();
+    }))
+;
 
 
 /*
