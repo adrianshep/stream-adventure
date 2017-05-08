00ee4e8b48399d134eb7610f33fb3bcd7f1b49f2
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-08 17:22:04 -0400

    5th exercise in stream-adventure

--- lines.js
+++ lines.js
@@ -1,17 +1,27 @@
-var through = require('through2');
-var split = require('split');
-var tr = through(function (buffer, encoding, next) {
-        this.push(buffer.toString().toUpperCase());
-        next();
-    });
+var through = require('through')
+var split = require('split')
+
+var alt = function (odd_fn, even_fn) {
+  var f = even_fn;
+  return function (buffer) {
+    f = (f === even_fn) ? odd_fn : even_fn
+    f.call(this, buffer)
+  }
+}
+
+var enqueue_after = function (string_fn) {
+  return function (buffer) {
+    var value = string_fn.call(buffer.toString()) + '\n'
+    this.queue(value)
+  }
+}
 
 process.stdin
-    .pipe(split())
-    .pipe(through2(function (line, _, next) {
-        console.dir(line.toString());
-        next();
-    }))
-;
+  .pipe(split())
+  .pipe(through(alt(
+    enqueue_after(String.prototype.toLowerCase),
+    enqueue_after(String.prototype.toUpperCase))))
+  .pipe(process.stdout)
 
 
 /*
