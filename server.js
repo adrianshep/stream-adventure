d14f83d6b25871a1dc622641430894db238a78c9
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-02 09:28:44 -0400

    5th exercise in stream-adventure

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        req.pipe(fs.createWriteStream('post.txt'));
+    }
+    res.end('beep boop\n');
+});
+server.listen(process.argv[2]);
 
 
 
